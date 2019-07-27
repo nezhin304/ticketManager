b84da66f1d9ee4d918edbd988283b9fef4adb302
Your Name <you@example.com>
2019-07-27 21:05:56 +0300

    added save ticket with old operator

--- src/main/java/com/example/ticketManager/repository/OperatorRepo.java
+++ src/main/java/com/example/ticketManager/repository/OperatorRepo.java
@@ -2,6 +2,10 @@ package com.example.ticketManager.repository;
 
 import com.example.ticketManager.entity.Operator;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface OperatorRepo extends JpaRepository<Operator, Long> {
+
+    @Query("select op.id, op.name from Operator op")
+    Long getOperatorIdByName(String name);
 }
--- src/test/java/com/example/ticketManager/TicketManagerApplicationTests.java
+++ src/test/java/com/example/ticketManager/TicketManagerApplicationTests.java
@@ -15,6 +15,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -28,25 +29,26 @@ public class TicketManagerApplicationTests {
 	@Test
 	public void contextLoads() throws ParseException {
 
-		Ticket ticket = new Ticket();
-		ticket.setDateOpen(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2019-07-27 12:00:00"));
-		ticket.setTtNumber("TT123456789");
-		Operator operator = new Operator();
-		operator.setName("BITAF");
-		List<Ticket> tickets = new ArrayList<>();
-		tickets.add(ticket);
-		operator.setTickets(tickets);
-		ticket.setOperator(operator);
+//		Ticket ticket = new Ticket();
+//		ticket.setDateOpen(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2019-07-27 12:00:00"));
+//		ticket.setTtNumber("TT123456789");
+//		Operator operator = new Operator();
+//		operator.setName("BITAF");
+//		List<Ticket> tickets = new ArrayList<>();
+//		tickets.add(ticket);
+//		operator.setTickets(tickets);
+//		ticket.setOperator(operator);
 
 //		operatorRepo.save(operator);
-		ticketRepo.save(ticket);
+//		ticketRepo.save(ticket);
 
 
 		Ticket ticket1 = new Ticket();
 		ticket1.setDateOpen(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2019-07-27 15:00:00"));
 		ticket1.setTtNumber("TT090897867665");
-
-		ticket1.setOperator(operator);
+		Long operatorId = operatorRepo.getOperatorIdByName("BITAF");
+		Operator operator1 = operatorRepo.getOne(operatorId);
+		ticket1.setOperator(operator1);
 		ticketRepo.save(ticket1);
 
 	}
